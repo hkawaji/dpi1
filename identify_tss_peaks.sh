1390b747f04e2f169c182d477c8af8ee2646fb05
KAWAJI, Hideya <h.kawaji@gmail.com>
2016-09-20 06:00:59 +0900

    bug fix: PATH environmental variable was not passed successfully over qsub.

--- identify_tss_peaks.sh
+++ identify_tss_peaks.sh
@@ -104,7 +104,7 @@ export curr_dir=$(pwd)
 installed_dir=$(dirname $0)
 
 ### prep for qsub
-qsub_v="PATH"
+qsub_v="PATH_OVERRIDE=${PATH}"
 for k in curr_dir installed_dir decomposition \
          dpi_genome dpi_ctss_path dpi_out_path \
          dpi_cluster_dist dpi_ctss_max_counts_threshold  \
--- scripts/qsub.sh
+++ scripts/qsub.sh
@@ -2,6 +2,10 @@
 
 #$ -l virtual_free=4G,mem_free=4G
 
+if [ "${PATH_OVERRIDE}" != "" ]; then
+  PATH=${PATH_OVERRIDE}
+fi
+
 echo started... $RAKE_TARGET
 rake $RAKE_TARGET
 echo ...finished.
